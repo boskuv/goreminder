acabcdecab39b4569b8c2cdd373779f1ed68a1d0
boskuv <boris_kv@bk.ru>
2025-01-14 22:25:34 +0300

    fix(models): update messenger types

--- internal/models/messenger.go
+++ internal/models/messenger.go
@@ -10,10 +10,10 @@ type Messenger struct {
 }
 
 type MessengerRelatedUser struct {
-	ID          int       `json:"-"`
-	UserID      *int      `json:"user_id"`
-	MessengerID *int      `json:"messenger_id"`
-	ChatID      string    `json:"chat_id"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int64      `db:"id" json:"-"`
+	UserID      *int64     `db:"user_id" json:"user_id"`
+	MessengerID *int64     `db:"messenger_id" json:"messenger_id"`
+	ChatID      string     `db:"chat_id" json:"chat_id"`
+	CreatedAt   time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt   *time.Time `db:"updated_at" json:"updated_at"`
 }
