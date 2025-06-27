2f96024c392d860054974651c73e9f8bda450c1d
boskuv <boris_kv@bk.ru>
2025-06-27 18:22:42 +0300

    refactor: update cases for User Service

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -7,51 +7,50 @@ import (
 	"github.com/pkg/errors"
 )
 
-// UserService handles user-related business logic.
+// UserService defines methods for user-related business logic
 type UserService struct {
 	userRepo repository.UserRepository
 }
 
-// NewUserService creates a new instance of UserService.
+// NewUserService creates a new instance of UserService
 func NewUserService(userRepo repository.UserRepository) *UserService {
 	return &UserService{userRepo: userRepo}
 }
 
-// CreateUser creates a new user in the system.
+// CreateUser implements BL of adding new user
 func (s *UserService) CreateUser(user *models.User) (int64, error) {
-	// Perform some validation before creating the user
-	if user.Name == "" || user.Email == "" || user.PasswordHash == "" {
+	// perform some validation before creating the user
+	if user.Name == "" {
 		return 0, errors.WithStack(errors.New("user data is incomplete"))
 	}
 
-	// Call the repository to insert the user into the database
 	userID, err := s.userRepo.CreateUser(user)
 	if err != nil {
-		return 0, err
+		return 0, errors.WithStack(err)
 	}
 
 	return userID, nil
 }
 
-// GetUser retrieves a user by its ID
+// GetUser implements BL of retrieving existing user by its id
 func (s *UserService) GetUser(userID int64) (*models.User, error) {
 	user, err := s.userRepo.GetUserByID(userID)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 
 	return user, nil
 }
 
-// UpdateUser retrieves an existing user by its ID and updates it
+// UpdateUser implements BL of updating user by id
 func (s *UserService) UpdateUser(userID int64, updateRequest *models.UserUpdateRequest) (*models.User, error) {
-	// Check if the task exists
+	// check if the user exists
 	user, err := s.userRepo.GetUserByID(userID)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 
-	// Update the task fields (partial update)
+	// update the user fields (partial update)
 	if updateRequest.Name != nil {
 		user.Name = *updateRequest.Name
 	}
@@ -62,25 +61,25 @@ func (s *UserService) UpdateUser(userID int64, updateRequest *models.UserUpdateR
 		user.PasswordHash = *updateRequest.PasswordHash
 	}
 
-	// Save the updated task
+	// save the updated user
 	err = s.userRepo.UpdateUser(user)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
 
 	return user, nil
 }
 
-// DeleteUser deletes a user by its ID (soft delete)
+// DeleteUser implements BL of soft deleting user by id
 func (s *UserService) DeleteUser(userID int64) error {
 	_, err := s.userRepo.GetUserByID(userID)
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	err = s.userRepo.DeleteUser(userID)
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	return nil
