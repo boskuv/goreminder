7c16987b92d5fc775d4cb98efbd4a239cab66a7d
boskuv <boris_kv@bk.ru>
2026-01-20 20:12:08 +0300

    refactor: update custom http errs

--- internal/errors/http.go
+++ internal/errors/http.go
@@ -6,9 +6,5 @@ var (
 	ErrNotFound            = errors.New("no data found matching criteria") // 404
 	ErrValidation          = errors.New("invalid input data")              // 400
 	ErrUnprocessableEntity = errors.New("unprocessable entity")            // 422
-// Forbidden 403
-// Unauthorized 401
-// Method Not Allowed 405
-// Conflict 409
-// Unprocessable Entity 422
+	ErrConflict            = errors.New("conflict")                        // 409
 )
